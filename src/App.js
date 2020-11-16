85ecbd6a543dab581ebfa2081928c18aa745497b
dominicmartincruz <dominicmartincruz@gmail.com>
2020-11-16 17:26:35 +0800

    updated app

--- src/App.js
+++ src/App.js
@@ -23,6 +23,12 @@ function App() {
     }
   }
 
+  function convertTime(unixTime, offset){
+    let dt = new Date((unixTime * 1000 + (offset * 1000)) - 28800000).toLocaleTimeString()
+    return dt
+  }
+
+
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -37,6 +43,9 @@ function App() {
 
   return (
     <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app '}>
+      <div className="forekastr">
+        <h1>f</h1><h1>o</h1><h1>r</h1><h1>e</h1><h1>k</h1><h1>a</h1><h1>s</h1><h1>t</h1><h1>r</h1>
+      </div>
       <main>
         <div className="search-box">
           <input
@@ -48,10 +57,12 @@ function App() {
             onKeyPress={search}
           />
         </div>
+        <hr/>
         {(typeof weather.main != "undefined") ? (
           <div>
             <div className="location-box">
               <div className="location">{weather.name}, {weather.sys.country}</div>
+              <p className="latLang">lat: {weather.coord.lat}, long: {weather.coord.lon}</p>
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
 
@@ -59,10 +70,36 @@ function App() {
               <div className="temp">
                 {Math.round(weather.main.temp)}°c
               </div>
-              <div className="weather">{weather.weather[0].main}</div>
+              <div className="weather">{weather.weather[0].description}</div>
+              <div className="itemsBox">
+                <div className="items">
+                  <span className="iconify iconify2" data-icon="wi-humidity" data-inline="false"></span>
+                  <p className="itemsTitle">humidity:</p>
+                  <p className="itemsMargin">{weather.main.humidity}%</p>
+                  <span className="iconify iconify2" data-icon="carbon:wind-stream" data-inline="false"></span>
+                  <p className="itemsTitle">pressure:</p>
+                  <p>{weather.main.pressure} hPa</p>
+                  <span className="iconify iconify2" data-icon="la-wind" data-inline="false"></span>
+                  <p className="itemsTitle">wind:</p>
+                  <p className="itemsMargin">{weather.wind.speed} km/h</p>
+                  <span className="iconify iconify2" data-icon="wi:wind-direction-ne" data-inline="false"></span>
+                  <p className="itemsTitle">direction:</p>
+                  <p>{weather.wind.deg}°</p>
+                </div>
+                <div className="items">
+                  <span className="iconify" data-icon="vaadin-sun-rise" data-inline="false"></span>
+                  <p>{convertTime(weather.sys.sunrise, weather.timezone)}</p>
+                  <span className="iconify" data-icon="vaadin-sun-down" data-inline="false"></span>
+                  <p>{convertTime(weather.sys.sunset, weather.timezone)}</p>
+                </div>
+              </div>
             </div>
           </div>
-        ) : ('')}
+        ) : (
+          <div className="globe">
+            <img src="https://i.gifer.com/3IsN.gif"></img>
+          </div>
+        )}
       </main>
     </div>
   );
